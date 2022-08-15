317ebdf5676c648aa9a2585d2257af148f594138
amslmm <amlsmm0@gmail.com>
2022-08-15 11:51:09 -0300

    Adicionado gitignore, ajustado arquitetura

---
+++ backend/src/main/java/com/br/backend/ProjetoFinal/ProjetoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.br.backend.ProjetoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/br/backend/ProjetoFinal/ProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.backend.ProjetoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
